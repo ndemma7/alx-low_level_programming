b419b4cb5963e9e200048112bc76bf81e215cc89
Emmanuel Ndagijimana <ndemma7@gmail.com>
2022-09-11 14:41:14 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,11 @@
  *
  * Description: using the main function
  * this program prints "programming is like building a multilingual puzzle"
+ *
  * Return: 0
  */
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
