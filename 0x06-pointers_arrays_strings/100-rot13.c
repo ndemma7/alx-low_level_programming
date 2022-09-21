91ec9a2bfeaaa6f105ca8e763a29369a241f6ec6
ndemma7 <ndemma7@gmail.com>
2022-09-21 13:34:39 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -9,38 +9,26 @@
 
 char *rot13(char *s)
 {
-
 	int a, b;
-
-
 
 	char test[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 	char res[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-
-
 	for (a = 0; s[a] != '\0'; a++)
 
 	{
-
 		for (b = 0; test[b] != '\0'; b++)
 
 		{
-
 			if (s[a] == test[b])
 
 			{
-
 				s[a] = res[b];
 
 				break;
-
 			}
-
 		}
-
 	}
-
 	return (s);
-
+}
