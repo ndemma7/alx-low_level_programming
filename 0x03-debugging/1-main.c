bd226e9d3aae072fa04f9c9399183f365b378aa5
ndemma7 <ndemma7@gmail.com>
2022-09-22 04:28:03 -0700

    You do not have to compile with -Wall -Werror -Wextra -pedantic for this task

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,19 +7,13 @@
  */
 
 int main(void)
+
 {
 
 	int i;
 
-
-
 	printf("Infinite loop incoming :(\n");
-
-
-
 	i = 0;
-
-
 
 	/**
 	* while (i < 10)
@@ -29,6 +23,5 @@ int main(void)
 	*/
 
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
