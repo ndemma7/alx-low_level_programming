1ea24c2051733e2ab639c0abf8eb397e585c924a
ndemma7 <ndemma7@gmail.com>
2022-09-22 10:43:56 -0700

    do hard things

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Test function for positive or negative
+ * main - tests function that prints if integer is positive or negative
  * Return: 0
  */
 
@@ -13,5 +13,6 @@ int main(void)
 	i = 0;
 
 	positive_or_negative(i);
+
 	return (0);
 }
